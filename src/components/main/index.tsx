6617ecbaf85687607bc0943359b17663af7c12ed
lunashp <hijnshp@gmail.com>
2024-11-06 22:54:28 +0900

    fix: 메인 위치변경

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -7,8 +7,8 @@ const Main: React.FC = () => {
   return (
     <div className="flex flex-col justify-center items-start gap-16 max-w-2xl border-gray-200 dark:border-gray-700 mx-auto pb-16">
       <Bio />
-      <Skills />
       <Projects />
+      <Skills />
     </div>
   );
 };
